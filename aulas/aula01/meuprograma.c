84d32cb76593a8b4c42004088b44da592459ca29
joaovitorjvc <25529744-joaovitorjvc@users.noreply.replit.com>
2023-08-24 00:46:08 +0000

    arquivos da aula 1

---
+++ aulas/aula01/meuprograma.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  printf("ola pessoal!\n");
+  printf("a raiz quadrada de 2 e %f\n", sqrt(2));
+  return 0;
+}
---
+++ praticas/praticas01/questao01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+  float nota_a1 = 5.5f;
+  float nota_a2 = 4.0f;
+
+  float media = 0.4 * nota_a1 + 0.6 * nota_a2;
+  printf("a media é %f\n", media);
+
+  return 0;
+}
